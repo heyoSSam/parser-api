3d41307441d01e85e319c5fd7c97c24612cce5b0
heyoSSam <heyossam11@gmail.com>
2025-03-17 13:56:43 +0500

    add read document text

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"parser-api/config"
+	"parser-api/internal/reader"
 )
 
 func main() {
@@ -11,6 +13,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
+
+	text, err := reader.ReadPDF("../")
+	if err != nil {
+		log.Fatalf("Failed to read pdf: %v", err)
+	}
+	fmt.Println(text)
 
 	r := gin.Default()
 
---
+++ internal/reader/reader.go
@@ -0,0 +1,22 @@
+package reader
+
+import (
+	"bytes"
+	"github.com/ledongthuc/pdf"
+)
+
+func ReadPDF(path string) (string, error) {
+	f, r, err := pdf.Open(path)
+
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	b, err := r.GetPlainText()
+	if err != nil {
+		return "", err
+	}
+	buf.ReadFrom(b)
+	return buf.String(), nil
+}
