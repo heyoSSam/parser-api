11cda7e95bddfa48e496cf39247ab47870d30dfc
heyoSSam <heyossam11@gmail.com>
2025-03-17 14:46:50 +0500

    init csv and sql logic

---
+++ internal/schema/constants.go
@@ -0,0 +1,87 @@
+package schema
+
+var Skeleton = `
+	CREATE TABLE IF NOT EXISTS codes (
+		code_id SERIAL PRIMARY KEY,
+		code VARCHAR(255),
+		version INTEGER,
+		status BOOLEAN,
+		actual_date DATE,
+		previous_date DATE
+	);
+	
+	CREATE TABLE IF NOT EXISTS part (
+		part_id SERIAL PRIMARY KEY,
+		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
+		code VARCHAR(255),
+		version INTEGER,
+		status BOOLEAN,
+		actual_date DATE,
+		previous_date DATE
+	);
+	
+	CREATE TABLE IF NOT EXISTS sections (
+		section_number SERIAL PRIMARY KEY,
+		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
+		section VARCHAR(255),
+		version INTEGER,
+		status BOOLEAN,
+		actual_date DATE,
+		previous_date DATE
+	);
+	
+	CREATE TABLE IF NOT EXISTS paragraphs (
+		paragraph_id SERIAL PRIMARY KEY,
+		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER REFERENCES sections(section_number) ON DELETE CASCADE,
+		paragraph VARCHAR(255),
+		version INTEGER,
+		status BOOLEAN,
+		actual_date DATE,
+		previous_date DATE
+	);
+	
+	CREATE TABLE IF NOT EXISTS articles (
+		article_number SERIAL PRIMARY KEY,
+		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER REFERENCES sections(section_number) ON DELETE CASCADE,
+		paragraph_id INTEGER REFERENCES paragraphs(paragraph_id) ON DELETE CASCADE,
+		article VARCHAR(255),
+		version INTEGER,
+		status BOOLEAN,
+		actual_date DATE,
+		previous_date DATE
+	);
+	
+	CREATE TABLE IF NOT EXISTS clauses (
+		clause_number SERIAL PRIMARY KEY,
+		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER REFERENCES sections(section_number) ON DELETE CASCADE,
+		paragraph_id INTEGER REFERENCES paragraphs(paragraph_id) ON DELETE CASCADE,
+		article_number INTEGER REFERENCES articles(article_number) ON DELETE CASCADE,
+		clause VARCHAR(255),
+		version INTEGER,
+		status BOOLEAN,
+		actual_date DATE,
+		previous_date DATE
+	);
+	
+	CREATE TABLE IF NOT EXISTS sub_clauses (
+		sub_clause_number SERIAL PRIMARY KEY,
+		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER REFERENCES sections(section_number) ON DELETE CASCADE,
+		paragraph_id INTEGER REFERENCES paragraphs(paragraph_id) ON DELETE CASCADE,
+		article_number INTEGER REFERENCES articles(article_number) ON DELETE CASCADE,
+		clause_number INTEGER REFERENCES clauses(clause_number) ON DELETE CASCADE,
+		sub_clause VARCHAR(255),
+		version INTEGER,
+		status BOOLEAN,
+		actual_date DATE,
+		previous_date DATE
+	);
+`
