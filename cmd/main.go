63280257ed89377a70a393cc2ae5f0de31d0c92d
heyoSSam <heyossam11@gmail.com>
2025-03-18 11:22:59 +0500

    sql checkpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"log"
-	"parser-api/config"
-	"parser-api/internal/reader"
+	"parser-api/internal/schema"
 )
 
 func main() {
-	cfg, err := config.LoadConfig("../config.yaml")
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	text, err := reader.ReadPDF("../")
-	if err != nil {
-		log.Fatalf("Failed to read pdf: %v", err)
-	}
-	fmt.Println(text)
-
-	r := gin.Default()
-
-	log.Printf("Server running on port " + cfg.Server.Port)
-	r.Run(":" + cfg.Server.Port)
+	schema.Inserts()
+	//cfg, err := config.LoadConfig("../config.yaml")
+	//if err != nil {
+	//	log.Fatalf("Failed to load config: %v", err)
+	//}
+	//
+	//r := gin.Default()
+	//
+	//log.Printf("Server running on port " + cfg.Server.Port)
+	//r.Run(":" + cfg.Server.Port)
 }
