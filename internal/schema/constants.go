d959b7f8ef3efa0ac08011b045e8af7782188430
heyossam <heyossam11@gmail.com>
2025-03-19 15:04:52 +0500

    sql fix

--- internal/schema/constants.go
+++ internal/schema/constants.go
@@ -2,71 +2,90 @@ package schema
 
 var Skeleton = `
 	CREATE TABLE IF NOT EXISTS codes (
-		code_id SERIAL PRIMARY KEY,
+		code_id INTEGER UNSIGNED PRIMARY KEY,
 		code VARCHAR(255),
 		version INTEGER,
 		status BOOLEAN,
-		actual_date DATE,
-		previous_date DATE
+		actual_date DATETIME,
+		previous_date DATETIME
 	);
 	
 	CREATE TABLE IF NOT EXISTS part (
-		part_id SERIAL PRIMARY KEY,
-		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER UNSIGNED PRIMARY KEY,
+		code_id INTEGER UNSIGNED REFERENCES codes(code_id) ON DELETE CASCADE,
 		code VARCHAR(255),
 		version INTEGER,
 		status BOOLEAN,
-		actual_date DATE,
-		previous_date DATE
+		actual_date DATETIME,
+		previous_date DATETIME
 	);
 	
 	CREATE TABLE IF NOT EXISTS sections (
-		section_number SERIAL PRIMARY KEY,
-		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
-		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER UNSIGNED PRIMARY KEY,
+		code_id INTEGER UNSIGNED REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER UNSIGNED REFERENCES part(part_id) ON DELETE CASCADE,
 		section VARCHAR(255),
 		version INTEGER,
 		status BOOLEAN,
-		actual_date DATE,
-		previous_date DATE
+		actual_date DATETIME,
+		previous_date DATETIME
 	);
 	
 	CREATE TABLE IF NOT EXISTS paragraphs (
-		paragraph_id SERIAL PRIMARY KEY,
-		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
-		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
-		section_number INTEGER REFERENCES sections(section_number) ON DELETE CASCADE,
+		paragraph_id INTEGER UNSIGNED PRIMARY KEY,
+		code_id INTEGER UNSIGNED REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER UNSIGNED REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER UNSIGNED REFERENCES sections(section_number) ON DELETE CASCADE,
 		paragraph VARCHAR(255),
 		version INTEGER,
 		status BOOLEAN,
-		actual_date DATE,
-		previous_date DATE
+		actual_date DATETIME,
+		previous_date DATETIME
 	);
 	
 	CREATE TABLE IF NOT EXISTS articles (
-		article_number SERIAL PRIMARY KEY,
-		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
-		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
-		section_number INTEGER REFERENCES sections(section_number) ON DELETE CASCADE,
-		paragraph_id INTEGER REFERENCES paragraphs(paragraph_id) ON DELETE CASCADE,
+		article_number INTEGER UNSIGNED PRIMARY KEY,
+		code_id INTEGER UNSIGNED REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER UNSIGNED REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER UNSIGNED REFERENCES sections(section_number) ON DELETE CASCADE,
+		paragraph_id INTEGER UNSIGNED REFERENCES paragraphs(paragraph_id) ON DELETE CASCADE,
 		article VARCHAR(255),
 		version INTEGER,
 		status BOOLEAN,
-		actual_date DATE,
-		previous_date DATE
+		actual_date DATETIME,
+		previous_date DATETIME
 	);
 	
 	CREATE TABLE IF NOT EXISTS clauses (
-		clause_number SERIAL PRIMARY KEY,
-		code_id INTEGER REFERENCES codes(code_id) ON DELETE CASCADE,
-		part_id INTEGER REFERENCES part(part_id) ON DELETE CASCADE,
-		section_number INTEGER REFERENCES sections(section_number) ON DELETE CASCADE,
-		paragraph_id INTEGER REFERENCES paragraphs(paragraph_id) ON DELETE CASCADE,
-		article_number INTEGER REFERENCES articles(article_number) ON DELETE CASCADE,
-		clause VARCHAR(255),
+		clause_id INTEGER UNSIGNED PRIMARY KEY,
+		clause_number INTEGER UNSIGNED,
+		code_id INTEGER UNSIGNED REFERENCES codes(code_id) ON DELETE CASCADE,
+		part_id INTEGER UNSIGNED REFERENCES part(part_id) ON DELETE CASCADE,
+		section_number INTEGER UNSIGNED REFERENCES sections(section_number) ON DELETE CASCADE,
+		paragraph_id INTEGER UNSIGNED REFERENCES paragraphs(paragraph_id) ON DELETE CASCADE,
+		article_number INTEGER UNSIGNED REFERENCES articles(article_number) ON DELETE CASCADE,
+		clause TEXT,
 		version INTEGER,
 		status BOOLEAN,
-		actual_date DATE,
-		previous_date DATE
+		actual_date DATETIME,
+		previous_date DATETIME
 	);
+
+	INSERT INTO codes (code_id, code, version, status, actual_date, previous_date)
+	VALUES (1, 'No Reference', 1, TRUE, NOW(), NULL);
+
+	INSERT INTO part (part_id, code_id, code, version, status, actual_date, previous_date) 
+	VALUES (0, 1, 'No Reference', 1, TRUE, NOW(), NULL);
+
+	INSERT INTO sections (section_number, code_id, part_id, section, version, status, actual_date, previous_date) 
+	VALUES (0, 1, 0, 'No Reference', 1, TRUE, NOW(), NULL);
+
+	INSERT INTO paragraphs (paragraph_id, code_id, part_id, section_number, paragraph, version, status, actual_date, previous_date) 
+	VALUES (0, 1, 0, 0, 'No Reference', 1, TRUE, NOW(), NULL);
+
+	INSERT INTO articles (article_number, code_id, part_id, section_number, paragraph_id, article, version, status, actual_date, previous_date) 
+	VALUES (0, 1, 0, 0, 0, 'No Reference', 1, TRUE, NOW(), NULL);
+
+	INSERT INTO clauses (clause_id, clause_number, code_id, part_id, section_number, paragraph_id, article_number, clause, version, status, actual_date, previous_date) 
+	VALUES (0, 0, 1, 0, 0, 0, 0, 'No Reference', 1, TRUE, NOW(), NULL);
 `
--- internal/schema/schema.go
+++ internal/schema/schema.go
@@ -23,43 +23,46 @@ func Inserts(path string) []string {
 	sentences := processing.SplitSentences(text)
 
 	var codeID, partID, sectionID, paragraphID, articleID, clauseID int
+	clauseCount := 0
+
 	for _, line := range sentences {
 		line = strings.TrimSpace(line)
 
 		if m, _ := processing.CodeRegex.FindStringMatch(line); m != nil {
 			codeTitle := m.String()
-			sql := fmt.Sprintf(`INSERT INTO codes (code_id, code, version, status, actual_date, previous_date) VALUES (1, %s, 1, TRUE, NOW(), NULL) RETURNING code_id;`, codeTitle)
+			sql := fmt.Sprintf(`UPDATE codes SET code = '%s', actual_date = NOW() WHERE code_id = 1;`, codeTitle)
 			sqlStatements = append(sqlStatements, sql)
 			codeID = 1
 		} else if m, _ := processing.PartRegex.FindStringMatch(line); m != nil {
 			partNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
 			partTitle := m.GroupByNumber(2).String()
 			partID = partNum
-			sql := fmt.Sprintf(`INSERT INTO part (part_id, code_id, code, version, status, actual_date, previous_date) VALUES (%d, %d, %s, 1, TRUE, NOW(), NULL) RETURNING part_id;`, partID, codeID, partTitle)
+			sql := fmt.Sprintf(`INSERT INTO part (part_id, code_id, code, version, status, actual_date, previous_date) VALUES (%d, %d, '%s', 1, TRUE, NOW(), NULL);`, partID, codeID, partTitle)
 			sqlStatements = append(sqlStatements, sql)
 		} else if m, _ := processing.SectionRegex.FindStringMatch(line); m != nil {
 			sectionNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
 			sectionTitle := m.GroupByNumber(2).String()
 			sectionID = sectionNum
-			sql := fmt.Sprintf(`INSERT INTO sections (section_number, code_id, part_id, section, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %s, 1, TRUE, NOW(), NULL) RETURNING section_number;`, sectionID, codeID, partID, sectionTitle)
+			sql := fmt.Sprintf(`INSERT INTO sections (section_number, code_id, part_id, section, version, status, actual_date, previous_date) VALUES (%d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, sectionID, codeID, partID, sectionTitle)
 			sqlStatements = append(sqlStatements, sql)
 		} else if m, _ := processing.ParagraphRegex.FindStringMatch(line); m != nil {
 			paragraphNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
 			paragraphTitle := m.GroupByNumber(2).String()
 			paragraphID = paragraphNum
-			sql := fmt.Sprintf(`INSERT INTO paragraphs (paragraph_id, code_id, part_id, section_number, paragraph, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %s, 1, TRUE, NOW(), NULL) RETURNING paragraph_id;`, paragraphID, codeID, partID, sectionID, paragraphTitle)
+			sql := fmt.Sprintf(`INSERT INTO paragraphs (paragraph_id, code_id, part_id, section_number, paragraph, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, paragraphID, codeID, partID, sectionID, paragraphTitle)
 			sqlStatements = append(sqlStatements, sql)
 		} else if m, _ := processing.ArticleRegex.FindStringMatch(line); m != nil {
 			articleNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
 			articleTitle := m.GroupByNumber(2).String()
 			articleID = articleNum
-			sql := fmt.Sprintf(`INSERT INTO articles (article_number, code_id, part_id, section_number, paragraph_id, article, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %d, %s, 1, TRUE, NOW(), NULL) RETURNING article_number;`, articleID, codeID, partID, sectionID, paragraphID, articleTitle)
+			sql := fmt.Sprintf(`INSERT INTO articles (article_number, code_id, part_id, section_number, paragraph_id, article, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, articleID, codeID, partID, sectionID, paragraphID, articleTitle)
 			sqlStatements = append(sqlStatements, sql)
 		} else if m, _ := processing.ClauseRegex.FindStringMatch(line); m != nil {
 			clauseNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
 			clauseTitle := m.GroupByNumber(2).String()
 			clauseID = clauseNum
-			sql := fmt.Sprintf(`INSERT INTO clauses (clause_number, code_id, part_id, section_number, paragraph_id, article_number, clause, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %d, %d, %s, 1, TRUE, NOW(), NULL) RETURNING clause_number;`, clauseID, codeID, partID, sectionID, paragraphID, articleID, clauseTitle)
+			clauseCount++;
+			sql := fmt.Sprintf(`INSERT INTO clauses (clause_id, clause_number, code_id, part_id, section_number, paragraph_id, article_number, clause, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, clauseCount, clauseID, codeID, partID, sectionID, paragraphID, articleID, clauseTitle)
 			sqlStatements = append(sqlStatements, sql)
 		}
 	}
