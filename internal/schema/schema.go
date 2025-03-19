f262699f114d4c9c6d24267ef20eff2ad80e2f8b
heyoSSam <heyossam11@gmail.com>
2025-03-19 17:18:13 +0500

    kazakh regex + sql update

--- internal/schema/schema.go
+++ internal/schema/schema.go
@@ -24,15 +24,17 @@ func Inserts(path string) []string {
 
 	var codeID, partID, sectionID, paragraphID, articleID, clauseID int
 	clauseCount := 0
+	codeStatus := false
 
 	for _, line := range sentences {
 		line = strings.TrimSpace(line)
 
-		if m, _ := processing.CodeRegex.FindStringMatch(line); m != nil {
+		if m, _ := processing.CodeRegex.FindStringMatch(line); m != nil && !codeStatus {
 			codeTitle := m.String()
 			sql := fmt.Sprintf(`UPDATE codes SET code = '%s', actual_date = NOW() WHERE code_id = 1;`, codeTitle)
 			sqlStatements = append(sqlStatements, sql)
 			codeID = 1
+			codeStatus = true
 		} else if m, _ := processing.PartRegex.FindStringMatch(line); m != nil {
 			partNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
 			partTitle := m.GroupByNumber(2).String()
@@ -61,11 +63,50 @@ func Inserts(path string) []string {
 			clauseNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
 			clauseTitle := m.GroupByNumber(2).String()
 			clauseID = clauseNum
-			clauseCount++;
+			clauseCount++
+			sql := fmt.Sprintf(`INSERT INTO clauses (clause_id, clause_number, code_id, part_id, section_number, paragraph_id, article_number, clause, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, clauseCount, clauseID, codeID, partID, sectionID, paragraphID, articleID, clauseTitle)
+			sqlStatements = append(sqlStatements, sql)
+		} else if m, _ := processing.KAZCodeRegex.FindStringMatch(line); m != nil && !codeStatus {
+			codeTitle := m.String()
+			sql := fmt.Sprintf(`UPDATE codes SET code = '%s', actual_date = NOW() WHERE code_id = 1;`, codeTitle)
+			sqlStatements = append(sqlStatements, sql)
+			codeID = 1
+			codeStatus = true
+		} else if m, _ := processing.KAZPartRegex.FindStringMatch(line); m != nil {
+			partNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
+			partTitle := m.GroupByNumber(2).String()
+			partID = partNum
+			sql := fmt.Sprintf(`INSERT INTO part (part_id, code_id, code, version, status, actual_date, previous_date) VALUES (%d, %d, '%s', 1, TRUE, NOW(), NULL);`, partID, codeID, partTitle)
+			sqlStatements = append(sqlStatements, sql)
+		} else if m, _ := processing.KAZSectionRegex.FindStringMatch(line); m != nil {
+			sectionNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
+			sectionTitle := m.GroupByNumber(2).String()
+			sectionID = sectionNum
+			sql := fmt.Sprintf(`INSERT INTO sections (section_number, code_id, part_id, section, version, status, actual_date, previous_date) VALUES (%d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, sectionID, codeID, partID, sectionTitle)
+			sqlStatements = append(sqlStatements, sql)
+		} else if m, _ := processing.KAZParagraphRegex.FindStringMatch(line); m != nil {
+			paragraphNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
+			paragraphTitle := m.GroupByNumber(2).String()
+			paragraphID = paragraphNum
+			sql := fmt.Sprintf(`INSERT INTO paragraphs (paragraph_id, code_id, part_id, section_number, paragraph, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, paragraphID, codeID, partID, sectionID, paragraphTitle)
+			sqlStatements = append(sqlStatements, sql)
+		} else if m, _ := processing.KAZArticleRegex.FindStringMatch(line); m != nil {
+			articleNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
+			articleTitle := m.GroupByNumber(2).String()
+			articleID = articleNum
+			sql := fmt.Sprintf(`INSERT INTO articles (article_number, code_id, part_id, section_number, paragraph_id, article, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, articleID, codeID, partID, sectionID, paragraphID, articleTitle)
+			sqlStatements = append(sqlStatements, sql)
+		} else if m, _ := processing.KAZClauseRegex.FindStringMatch(line); m != nil {
+			clauseNum, _ := strconv.Atoi(m.GroupByNumber(1).String())
+			clauseTitle := m.GroupByNumber(2).String()
+			clauseID = clauseNum
+			clauseCount++
 			sql := fmt.Sprintf(`INSERT INTO clauses (clause_id, clause_number, code_id, part_id, section_number, paragraph_id, article_number, clause, version, status, actual_date, previous_date) VALUES (%d, %d, %d, %d, %d, %d, %d, '%s', 1, TRUE, NOW(), NULL);`, clauseCount, clauseID, codeID, partID, sectionID, paragraphID, articleID, clauseTitle)
 			sqlStatements = append(sqlStatements, sql)
 		}
 	}
+
+	fmt.Println(sqlStatements)
 
 	return sqlStatements
 }
